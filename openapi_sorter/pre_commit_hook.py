cc696a308c852cbd466adc694a388d35dc94cfce
Sugiyama Tsuyoshi <sugiyama@fullmarks.co.jp>
2023-04-10 18:33:42 +0900

    Try directly access to argv

--- openapi_sorter/pre_commit_hook.py
+++ openapi_sorter/pre_commit_hook.py
@@ -5,10 +5,11 @@ import sys
 from openapi_sorter.openapi_sorter import OpenApiSorter
 
 
-def format_files(file_paths):
+def format_files():
+    file_paths = sys.argv[1:]
     for file_path in file_paths:
         OpenApiSorter().sort(input_file=file_path, is_overwrite=True)
 
 
 if __name__ == '__main__':
-    format_files(sys.argv[1:])
+    pass
